e1e30721535f245bd859d4be95a200ea51ed0a9f
Brian <briankeefe@umass.edu>
2019-12-02 15:03:33 +0000

    added a zip field

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Box, Typography, Grid } from '@material-ui/core'
+import { Box, Typography, Grid, Paper, TextField, Button } from '@material-ui/core'
 import "./style.scss";
 import Card from "./Card"
-import {blue} from "@material-ui/core/colors"
+import { blue } from "@material-ui/core/colors";
+import { useState } from "react";
 
 function App() {
+	const [zip, setZip] = useState(11111);
+	const [textField, setTextField] = useState(zip);
+
+	const changeZip = (n) => {
+		console.log(n);
+	}
+
+	const updateText = (e) => {
+		setTextField(e.target.value);
+	}
+
+	const updateZip = (e) => {
+		setZip(textField);
+	}
+
 	return (
-		<Box className="home-page" style={{backgroundColor: blue[200]}}>
+		<Box className="home-page" style={{ backgroundColor: blue[200] }}>
 			<Box className="next-box" pt={10}>
+				<Paper>
+					<Box flex justifyContent="center">
+						<Typography>Zipcode: {zip}</Typography>
+						<TextField onChange={updateText}></TextField>
+						<Button onClick={updateZip}>Change</Button>
+					</Box>
+				</Paper>
 				<Grid container justify="center" spacing={2}>
 					<Grid item>
-						<Card temp={100}/>
+						<Card temp={100} />
 					</Grid>
 					<Grid item>
-						<Card/>
+						<Card />
 					</Grid>
 					<Grid item>
-						<Card/>
+						<Card />
 					</Grid>
 					<Grid item>
-						<Card/>
+						<Card />
 					</Grid>
 					<Grid item>
-						<Card/>
+						<Card />
 					</Grid>
 				</Grid>
 			</Box>
