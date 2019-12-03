f1fe7d4c082c2221e7d80c1eda79c1e4a78c55a7
Brian <briankeefe@umass.edu>
2019-12-03 20:13:11 +0000

    Duplicate packages pt2

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { green, blue } from "@material-ui/core/colors";
+import { Box } from "@material-ui/core";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blue,
+        secondary: green,
+    },
+});
+
+const routing = (
+    <Box className="outermost">
+        <ThemeProvider theme={theme}>
+            <Router>
+                <div>
+                    <Route path="/" component={App} />
+                    <Route path="/today" component={App} />
+                    <Route path="/tomorrow" component={App} />
+                </div>
+            </Router>
+        </ThemeProvider>
+    </Box>
+);
+ReactDOM.render(routing, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/layout/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {AppBar, Toolbar, Typography} from "@material-ui/core";
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const toolbar = (
+	<AppBar position="static">
+		<Toolbar>
+			<IconButton edge="start" color="inherit" aria-label="menu">
+				<MenuIcon />
+			</IconButton>
+			<Typography variant="h6">
+				Snowday Calculator 2.0
+          	</Typography>
+		</Toolbar>
+	</AppBar>
+);
+
+export default toolbar;
