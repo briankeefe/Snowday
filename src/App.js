877d18ca02fc4bb87975c0bed07cb856aea247fd
Brian <briankeefe@umass.edu>
2019-12-04 14:30:40 +0000

    added to card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 import {
-  Box,
-  Grid,
-  Paper,
-  TextField,
-  Button,
-  Typography
+	Box,
+	Grid,
+	Paper,
+	TextField,
+	Button,
+	Typography
 } from "@material-ui/core";
 import "./style.scss";
 import Card from "./Card";
@@ -16,88 +16,101 @@ import Header from "./layout/header";
 import Axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faLocationArrow,
-  faMapMarker
+	faLocationArrow,
+	faMapMarker
 } from "@fortawesome/free-solid-svg-icons";
 
 const DEFAULTLOC = "01772";
 
 function App() {
-  const [zip, setZip] = useState(DEFAULTLOC);
-  const [theText, setTheText] = useState(zip);
-  const [nextSnow, setNextSnow] = useState(0);
-  const [textCode, setTextCode] = useState(DEFAULTLOC);
-  const [city, setCity] = useState();
+	const [zip, setZip] = useState(DEFAULTLOC);
+	const [theText, setTheText] = useState(zip);
+	const [nextSnow, setNextSnow] = useState(0);
+	const [textCode, setTextCode] = useState(DEFAULTLOC);
+	const [city, setCity] = useState();
 
-  const updateText = e => {
-    setTheText(e.target.value);
-  };
+	const updateText = e => {
+		setTheText(e.target.value);
+	};
 
-  const updateZip = () => {
-    setZip(theText);
-    call(theText);
-  };
+	const updateZip = () => {
+		setZip(theText);
+		call(theText);
+	};
 
-  const call = code => {
-    Axios.get("http://localhost:3001/weather", {
-      params: {
-        zip: code
-      }
-    })
-      .then(res => {
-        console.log(res.data);
-        if (!res.data.snow) {
-          setNextSnow(0);
-        } else {
-          console.log(res.data.snow);
-          setNextSnow(res.data.snow["1h"]);
-        }
-        setCity(res.data.name);
-      })
-      .then(() => {
-        setTextCode(code);
-      });
-  };
-  useEffect(() => {
-    call(DEFAULTLOC);
-    setTextCode(DEFAULTLOC);
-  }, []);
+	const call = code => {
+		Axios.get("http://localhost:3001/weather", {
+			params: {
+				zip: code
+			}
+		})
+			.then(res => {
+				console.log(res.data);
+				if (!res.data.snow) {
+					setNextSnow(0);
+				} else {
+					console.log(res.data.snow);
+					setNextSnow(res.data.snow["1h"]);
+				}
+				setCity(res.data.name);
+			})
+			.then(() => {
+				setTextCode(code);
+			});
+	};
+	useEffect(() => {
+		call(DEFAULTLOC);
+		//setTextCode(DEFAULTLOC);
+	}, []);
 
-  return (
-    <Box className="home-page" style={{ backgroundColor: blue[200] }}>
-      {Header}
-      <Box className="next-box">
-        <Box mb={2}>
-          <Grid container justify="center">
-            <Grid item lg={2} sm={3} xs={8}>
-              <Paper>
-                <Box mt={2} pt={2} display="flex" justifyContent="center">
-                  <FontAwesomeIcon icon={faMapMarker} />
-                  <Typography style={{ marginLeft: "8px" }}>
-                    {textCode}
-                  </Typography>
-                </Box>
-                <Box m={2} mt={0} p={1} display="flex" justifyContent="center">
-                  <TextField
-                    style={{ width: "60%" }}
-                    label="Zipcode"
-                    variant="filled"
-                    onChange={updateText}
-                  ></TextField>
-                  <Button onClick={updateZip}>Change</Button>
-                </Box>
-              </Paper>
-            </Grid>
-          </Grid>
-        </Box>
-        <Grid container spacing={2} justify="center">
-          <Grid item xs={12} sm={8} md={2}>
-            <Card city={city} snow={nextSnow} temp={100} />
-          </Grid>
-        </Grid>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box className="home-page" style={{ backgroundColor: blue[200] }}>
+			{Header}
+			<Box className="next-box">
+				<Box mb={2}>
+					<Grid container justify="center">
+						<Grid item lg={2} sm={3} xs={8}>
+							<Paper>
+								<Box
+									mt={2}
+									pt={2}
+									display="flex"
+									justifyContent="center"
+								>
+									<FontAwesomeIcon icon={faMapMarker} />
+									<Typography style={{ marginLeft: "8px" }}>
+										{textCode}
+									</Typography>
+								</Box>
+								<Box
+									m={2}
+									mt={0}
+									p={1}
+									display="flex"
+									justifyContent="center"
+								>
+									<TextField
+										style={{ width: "60%" }}
+										label="Zipcode"
+										variant="filled"
+										onChange={updateText}
+									></TextField>
+									<Button variant="outlined" onClick={updateZip}>
+										Change
+									</Button>
+								</Box>
+							</Paper>
+						</Grid>
+					</Grid>
+				</Box>
+				<Grid container spacing={2} justify="center">
+					<Grid item xs={12} sm={8} md={2}>
+						<Card city={city} snow={nextSnow} temp={100} />
+					</Grid>
+				</Grid>
+			</Box>
+		</Box>
+	);
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,47 +1,54 @@
 import React from "react";
-import { Box, Card, Typography, Grid } from "@material-ui/core";
+import {
+	Box,
+	Card,
+	Typography,
+	Grid,
+	CardContent,
+	CardHeader
+} from "@material-ui/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloud, faSnowflake } from "@fortawesome/free-solid-svg-icons";
 const THRESHHOLD = 0.1;
 
 const card = props => {
-  let snow = props.snow;
-  let city = props.city;
-  let icon;
-  if (snow > THRESHHOLD) {
-    icon = <FontAwesomeIcon className="icon" icon={faSnowflake} />;
-  } else {
-    icon = <FontAwesomeIcon className="icon" icon={faCloud} />;
-  }
-  let chance;
-  if (snow > 10) {
-    chance = 100;
-  } else if (snow > 5) {
-    chance = 60;
-  } else if (snow > 3) {
-    chance = 30;
-  } else {
-    chance = 0;
-  }
-  return (
-    <Box>
-      <Card className="component-card">
-        <Box p={3}>
-          <Grid container className="cc-box" justify="center">
-            <Typography>{city}</Typography>
-          </Grid>
-          <Grid container className="cc-box" justify="center">
-            <Box className="fontawesome">{icon}</Box>
-          </Grid>
-          <Grid container className="cc-box" justify="center">
-            <Typography>Snowfall: {snow} in.</Typography>
-          </Grid>
-          <Grid container className="cc-box" justify="center">
-            <Typography>Chance of snowday: {chance}%</Typography>
-          </Grid>
-        </Box>
-      </Card>
-    </Box>
-  );
+	let snow = props.snow;
+	let city = props.city;
+	let icon;
+	if (snow > THRESHHOLD) {
+		icon = <FontAwesomeIcon className="icon" icon={faSnowflake} />;
+	} else {
+		icon = <FontAwesomeIcon className="icon" icon={faCloud} />;
+	}
+	let chance;
+	if (snow > 10) {
+		chance = 100;
+	} else if (snow > 5) {
+		chance = 60;
+	} else if (snow > 3) {
+		chance = 30;
+	} else {
+		chance = 0;
+	}
+	return (
+		<Box className="outer-card">
+			<Card className="component-card">
+				<CardHeader title={city} />
+				<CardContent>
+					<Box p={3}>
+						<Grid container className="cc-box" justify="center">
+							<Box className="fontawesome">{icon}</Box>
+						</Grid>
+						<Grid container className="cc-box" justify="center">
+							<Typography>Snowfall: {snow} in.</Typography>
+						</Grid>
+						<Grid container className="cc-box" justify="center">
+							<Typography>Chance of snowday: {chance}%</Typography>
+						</Grid>
+					</Box>
+				</CardContent>
+			</Card>
+		</Box>
+	);
 };
 export default card;
